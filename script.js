b9aeff9e127f4af957c09de8ebb6575e09cbe18d
Nicolas Araújo <nicolasaraujo04@gmail.com>
2022-09-02 16:46:07 -0300

    Layout-estatico da Tela 2 (Finalizado)

--- script.js
+++ script.js
@@ -2,12 +2,34 @@
 const API= "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
 
 //Essa é a parte em que os quizzes do servidor são renderizados na tela Inicial. 
-function buscarQuizzServidor(){
+function buscarQuizzesServidor(){
     const promessa= axios.get(API);
     promessa.then(renderizarQuizzServidor);
 }
-buscarQuizzServidor()
+buscarQuizzesServidor()
 
 function renderizarQuizzServidor(res){
+    const quizzServidor= document.querySelector(".quizz-servidor .todos-quizzes");
 
+    quizzServidor.innerHTML= ""
+
+    for(let i=0; i<res.data.length; i++){
+        quizzServidor.innerHTML+= `
+            <figure id=${res.data[i].id} onclick="exibirquizz(${res.data[i].id})">
+                <img src=${res.data[i].image} alt="quizz">
+                <div></div>
+                <figcaption>${res.data[i].title}</figcaption>
+            </figure>
+        `
+    }
+    console.log(res.data[0])//
+}
+
+//Essa é a parte em que acorre a troca entre a tela inicial e a exibição do quizz.
+function exibirquizz(idDoQuizz){
+    const telaInicial= document.querySelector(".tela1-inicial");
+    const telaExibiçaoQuizz= document.querySelector(".tela2-exibir_quizz");
+
+    telaInicial.classList.add("oculto");
+    telaExibiçaoQuizz.classList.remove("oculto");
 }
